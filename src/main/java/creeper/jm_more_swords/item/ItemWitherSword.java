4689a68cc0d50a12a62cfabd3a9b33469d12a23c
herobrine19 <1909977364@qq.com>
2020-12-23 23:46:23 +0800

    增加了凋零之剑使周围怪物凋零的效果

--- src/main/java/creeper/jm_more_swords/item/ItemWitherSword.java
+++ src/main/java/creeper/jm_more_swords/item/ItemWitherSword.java
@@ -1,7 +1,10 @@
 package creeper.jm_more_swords.item;
 
-import ibxm.Player;
+import com.google.common.base.Predicate;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
@@ -9,7 +12,9 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
-import net.minecraftforge.fml.common.FMLCommonHandler;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Created by jin on 2020/12/21 3:27 PM
@@ -20,35 +25,39 @@ public class ItemWitherSword extends ItemSwordBase
         super(name, material);
     }
 
-
     @Override
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
         if(isSelected && entityIn instanceof EntityPlayer)
         {
-            //((EntityPlayer)entityIn).addPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 2, 0, false, false));
-            String command0 = "/gamerule sendCommandFeedback false";
-            String command = "/particle smoke ~ ~ ~ 1 1 1 0.01 100";
-            String command1 = "/gamerule sendCommandFeedback true";
-            //String command = "/particle flame ~ ~1 ~ 0.2 0.2 0.2 0 1000";
-
-            if (worldIn instanceof WorldServer)
+            if (worldIn instanceof WorldServer)//身边黑色粒子效果
             {
                 WorldServer worldserver = (WorldServer)worldIn;
-
-
                 //worldserver.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, entityIn.posX, entityIn.posY, entityIn.posZ, 100, 1, 1, 1, 0.001);
-                worldserver.spawnParticle(EnumParticleTypes.REDSTONE, entityIn.posX, entityIn.posY, entityIn.posZ, 150, 1, 1.5, 1, 0.0001);//这里很奇怪，最后一个速度参数设置超过0.1粒子就变成彩色了
-
-
-
-
+                worldserver.spawnParticle(EnumParticleTypes.REDSTONE, entityIn.posX, entityIn.posY, entityIn.posZ, 120, 1, 1.5, 1, 0.0001);//这里很奇怪，最后一个速度参数设置超过0.1粒子就变成彩色了
             }
 
-            //if(!worldIn.isRemote)
-                //FMLCommonHandler.instance().getMinecraftServerInstance().commandManager.executeCommand(entityIn, command0);
-                //FMLCommonHandler.instance().getMinecraftServerInstance().commandManager.executeCommand(entityIn, command);
-                //FMLCommonHandler.instance().getMinecraftServerInstance().commandManager.executeCommand(entityIn, command1);
+            List<Entity> entityList = worldIn.getEntitiesInAABBexcluding(entityIn, entityIn.getEntityBoundingBox().grow(50), new Predicate<Entity>() {
+                @Override
+                public boolean apply(@Nullable Entity input) {
+                    if(input instanceof EntityMob)
+                        return true;
+                    if(input instanceof EntityLiving)
+                    {
+                        EntityLivingBase target = ((EntityLiving) input).getAttackTarget();
+                        if(target==entityIn)  // 如果攻击目标是玩家
+                            return true;
+                    }
+                    return false;
+                }
+            });
+
+            for(Entity mob: entityList)
+            {
+                ((EntityLiving)mob).addPotionEffect(new PotionEffect(MobEffects.WITHER, 5, 10, true, true));
+            }
         }
     }
+
+
 }
