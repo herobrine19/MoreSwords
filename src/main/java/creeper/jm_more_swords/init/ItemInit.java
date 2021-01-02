6d048c3986319a61253d8272e061a90eaf5952fd
herobrine19 <1909977364@qq.com>
2021-01-02 23:36:31 +0800

    加了疾风之剑

--- src/main/java/creeper/jm_more_swords/init/ItemInit.java
+++ src/main/java/creeper/jm_more_swords/init/ItemInit.java
@@ -16,4 +16,5 @@ public class ItemInit
     public static final Item FLAME_SWORD = new ItemFlameSword("flame_sword",SwordMaterial.FLAME);
     public static final Item ENDER_SWORD = new ItemEnderSword("ender_sword",SwordMaterial.ENDER);
     public static final Item LIGHT_SWORD = new ItemLightSword("light_sword",SwordMaterial.LIGHT);
+    public static final Item WIND_SWORD = new ItemWindSword("wind_sword",SwordMaterial.WIND);
 }
---
+++ src/main/java/creeper/jm_more_swords/item/ItemWindSword.java
@@ -0,0 +1,12 @@
+package creeper.jm_more_swords.item;
+
+/**
+ * Created by jin on 2020/12/26 4:01 PM
+ */
+public class ItemWindSword extends ItemSwordBase {
+    public ItemWindSword(String name, ToolMaterial material) {
+        super(name, material);
+    }
+
+    
+}
